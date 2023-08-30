3b013f9d9cdc9cc9cd49840d79b3765548873ec7
Mustafa Al-Shebeeb <ma00056@mix.wvu.edu>
2023-08-30 14:32:41 -0400

    Implemented a setFields method in the Row class. This way, the fields method will be updated with the new values passed to the method.

--- src/main/java/models/Row.java
+++ src/main/java/models/Row.java
@@ -4,4 +4,9 @@ import java.util.List;
 
 public record Row(String key, List<Object> fields) {
 
+	public Row setFields(List<Object> fields2) {
+		// TODO Auto-generated method stub
+		return new Row(key, fields2); // Create a new Row instance with updated fields
+	}
+
 }
