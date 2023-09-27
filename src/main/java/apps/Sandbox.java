b58b7a784213993f9e7e50b2dd57ef851300f3f3
Mustafa Al-Shebeeb <ma00056@mix.wvu.edu>
2023-09-27 16:51:17 -0400

    Old sandbox code for module 1. fixed alignment issues for toTabularView, fixed the hardcoding of table names

--- src/main/java/apps/Sandbox.java
+++ src/main/java/apps/Sandbox.java
@@ -2,83 +2,40 @@ package apps;
 
 import java.util.List;
 
-import types.HashTable;
+import types.SearchTable;
 
 public class Sandbox {
 	public static void main(String[] args) {
+		/*
+		 * TODO: Modify as needed to debug
+		 * or demonstrate arbitrary code.
+		 */
+
 		// Table 1: Employee Information
-		HashTable table1 = new HashTable("Companies", List.of("Table #", "Name", "Position"));
+		SearchTable table1 = new SearchTable("Companies", List.of("Table #", "Name", "Position"));
 
 		table1.put("162", List.of("Leidos", "CS Intern"));
 		table1.put("201", List.of("Terrasim", "GIS Consultant"));
 		table1.put("144", List.of("M&S", "IT Technician"));
 
-		System.out.println("Employer Information Table:");
-		System.out.println(table1.toTabularView(false));
-
-		// Create partitions based on filter targets
-		HashTable leidosEmployees = (HashTable) table1.filter("Leidos"); // Employees at Leidos
-		HashTable consultants = (HashTable) table1.filter("Terrasim"); // Consultants in all companies
-		HashTable tableMoreThan140 = (HashTable) table1.filter("M&S"); // Non-existent company
-
-		// Display partitions
-		System.out.println("\nPartition 1: Employers at Leidos");
-		System.out.println(leidosEmployees.toTabularView(false));
-
-		System.out.println("\nPartition 2: Consultants in All Companies");
-		System.out.println(consultants.toTabularView(false));
-
-		System.out.println("\nPartition 3: Table# larger than 140");
-		System.out.println(tableMoreThan140.toTabularView(false));
+		System.out.println(table1);
 
 		// Table 2: Product Catalog
-		HashTable table2 = new HashTable("ProductCatalog", List.of("ProductID", "Name", "Price"));
+		SearchTable table2 = new SearchTable("ProductCatalog", List.of("ProductID", "Name", "Price"));
 
-		table2.put("GPU", List.of("RTX 3060", 400.0));
-		table2.put("CPU", List.of("Ryzen 5", 250.0));
-		table2.put("Case", List.of("NZXT H510", 90.0));
+		table2.put("P001", List.of("RTX 3060", 400.0));
+		table2.put("P002", List.of("Ryzen 5", 250.0));
+		table2.put("P003", List.of("NZXT H510", 90.0));
 
-		System.out.println("\nProduct Catalog Table:");
-		System.out.println(table2.toTabularView(false));
+		System.out.println(table2);
 
-		// Create partitions based on filter targets
-		HashTable highPriceProducts = (HashTable) table2.filter(400.0); // Products with a price above $300
-		HashTable ryzenProducts = (HashTable) table2.filter("CPU"); // Products with "Ryzen" in the name
-		HashTable lowPriceProducts = (HashTable) table2.filter(90.0); // Products with a price less than $100
-
-		// Display partitions
-		System.out.println("\nPartition 4: Products with Price Above $350");
-		System.out.println(highPriceProducts.toTabularView(false));
-
-		System.out.println("\nPartition 5: Products with that are CPU");
-		System.out.println(ryzenProducts.toTabularView(false));
-
-		System.out.println("\nPartition 6: Products with Price below $100");
-		System.out.println(lowPriceProducts.toTabularView(false));
-
-		// Table 3: Factions
-		HashTable table3 = new HashTable("Factions", List.of("Moral", "Name", "Game"));
+		// Table 3: Customer Data
+		SearchTable table3 = new SearchTable("Factions", List.of("Moral", "Name", "Game"));
 
 		table3.put("Evil", List.of("Institute", "Fallout 4"));
-		table3.put("Mixed", List.of("BOS", "Fallout 3"));
-		table3.put("Good", List.of("NCR", "Fallout NV"));
+		table3.put("Mixed", List.of("BOS", "Fallout 3, NV, 4"));
+		table3.put("Good", List.of("NCR", "Fallout 1, 2, NV"));
 
-		System.out.println("\nFactions Table:");
-		System.out.println(table3.toTabularView(false));
-
-		// Create partitions based on filter targets
-		HashTable evilFactions = (HashTable) table3.filter("Evil"); // Factions with moral alignment "Evil"
-		HashTable fallout4 = (HashTable) table3.filter("Fallout 3"); // Factions associated with Fallout games
-		HashTable caliAcronym = (HashTable) table3.filter("NCR"); // factions that are called " New California Repbulic"
-
-		// Display partitions
-		System.out.println("\nPartition 7: Factions with Moral Alignment 'Evil'");
-		System.out.println(evilFactions.toTabularView(false));
-
-		System.out.println("\nPartition 8: Factions Associated with 'Fallout 3' Games");
-		System.out.println(fallout4.toTabularView(false));
-
-		System.out.println("\nPartition 9: Factions named the 'New California Republic'");
-		System.out.println(caliAcronym.toTabularView(false));
+		System.out.println(table3);
 	}
 }
