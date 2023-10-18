ce80339304fdd5b8216e558de3d928eb444b3472
Ron Reaser <ron@reaser.org>
2023-10-18 15:50:48 -0400

    Release Module 4

---
+++ src/main/java/types/CSVTable.java
@@ -0,0 +1,81 @@
+package types;
+
+import java.util.Iterator;
+import java.util.List;
+
+import models.Row;
+import models.StoredTable;
+
+public class CSVTable implements StoredTable {
+	/*
+	 * TODO: For Module 4, finish this stub.
+	 */
+
+	public CSVTable(String name, List<String> columns) {
+		throw new UnsupportedOperationException();
+	}
+
+	public CSVTable(String name) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<Object> put(String key, List<Object> fields) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<Object> get(String key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<Object> remove(String key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int degree() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int size() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int hashCode() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<Row> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String name() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<String> columns() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String toString() {
+		throw new UnsupportedOperationException();
+	}
+}
